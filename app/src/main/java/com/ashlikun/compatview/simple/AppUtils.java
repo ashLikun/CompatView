bd5c53c3fc4b155d3834abb475e050fd8118d4bc
likun <496546144@qq.com>
2018-08-29 16:31:05 +0800

    优化

---
+++ app/src/main/java/com/ashlikun/compatview/simple/AppUtils.java
@@ -0,0 +1,18 @@
+package com.ashlikun.compatview.simple;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * 作者　　: 李坤
+ * 创建时间: 2018/8/27　17:58
+ * 邮箱　　：496546144@qq.com
+ * <p>
+ * 功能介绍：
+ */
+public class AppUtils {
+    public static Application app;
+    public static Context getApp() {
+        return app;
+    }
+}
